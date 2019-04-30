505d722b11745880633f870029c3faf04524422c
Joakim <jocke_hedman@hotmail.com>
2019-04-30 23:10:24 +0200

    hidden under dev fields

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ class App extends Component {
       <div className="main">
         <header>
           <h1>Joakim Hedman</h1>
-          <div className="job-titles">
+          {/* <div className="job-titles">
             <h3><a href="#">Web Developer</a></h3>
             <span>/</span>
             <h3><a href="#">Game Developer</a></h3>
-          </div>
+          </div> */}
           <div className="social-links">
             <a className="linkedin" href="https://www.linkedin.com/in/joakim-hedman/" target="_blank" rel="noopener noreferrer"><span>LinkedIn</span></a>
             <a className="twitter" href="https://twitter.com/zahhibb" target="_blank" rel="noopener noreferrer"><span>Twitter</span></a>
-            <StealthText className="stealth-text" pose={this.state.isVisible ? 'visible' : 'hidden'} >StealthText</StealthText>
+            {/* <StealthText className="stealth-text" pose={this.state.isVisible ? 'visible' : 'hidden'} >StealthText</StealthText> */}
           </div>
         </header>
         <div className="content">
