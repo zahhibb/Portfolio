e27020f13904b1276db4ac1e48b644ccf1fd9c97
Zahhibb <jocke_hedman@hotmail.com>
2021-04-04 14:01:45 +0200

    wip replacement master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function SocialButton({link, imgClass}) {
+  return (
+    <div className={"social-button " + imgClass}>
+      <a href={link}></a>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
-    return (      
+    return (
       <div className="main">
         <div className="content">
+          <h1>Joakim Hedman</h1>
+          <h2>Frontend developer & Technical Game Designer</h2>
+          <div className="socials">
+            <SocialButton link={"linkedin"} imgClass={"linkedin"} />
+            <SocialButton link={"twitter"} imgClass={"twitter"} />
+          </div>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import './index.css';
 
-import Header from './Header';
 import App from './App';
-import Other from './Other';
 
 const root = document.getElementById('root');
 const routing = (
 <Router>
-    <Route path="/" component={Header}></Route>
     <Route exact path="/" component={App}></Route>
-    <Route path="/other" component={Other}></Route>
 </Router>
 );
 
 
 ReactDOM.render(routing, root);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
