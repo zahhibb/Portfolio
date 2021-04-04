5870cbfc55153c126514590294f30fee453331d7
Zahhibb <jocke_hedman@hotmail.com>
2021-04-04 18:39:09 +0200

    text capitalization and text shadow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="main">
         <div className="content">
           <h1>Joakim Hedman</h1>
-          <h2>Frontend developer</h2><span>/</span><h2>Technical Game Designer</h2>
+          <h2>Frontend Developer</h2><span>/</span><h2>Technical Game Designer</h2>
           <div className="socials">
             <SocialButton link={"https://www.linkedin.com/in/joakim-hedman/"} imgClass={"linkedin"} anchorContent={"LinkedIn"} />
             <SocialButton link={"https://twitter.com/Zahhibb"} imgClass={"twitter"} anchorContent={"Twitter"} />
